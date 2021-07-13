aa459f866c9d438b00f9fbc485ac8f51e64cd1b8
Johan Quijano <johanqnms@gmail.com>
2021-07-12 20:03:27 -0500

    Nav

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import Header from "../components/Header";
+import Nav from "../components/Nav";
 
 //Componentes
 const router = async () => {
@@ -6,6 +7,8 @@ const router = async () => {
     //Components
     const header = document.querySelector('.header');
     header.innerHTML = await Header();
+    const nav = document.querySelector('.nav');
+    nav.innerHTML = await Nav();
 }
 
 export default router;
